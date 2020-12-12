7514b106c71438d4679dd72a3e7bb1111964c70d
André Luferat <contato@luferat.net>
2020-12-12 10:35:57 -0300

    Versão Web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCHNvR6_iRDOVWNW6QjdopXvbTmL8MQ584',
+    authDomain: 'spidertrack-67141.firebaseapp.com',
+    databaseURL: 'https://spidertrack-67141.firebaseio.com',
+    projectId: 'spidertrack-67141',
+    storageBucket: 'spidertrack-67141.appspot.com',
+    messagingSenderId: '1065214411932',
+    appId: '1:1065214411932:web:13f0aa4a6cc4596bd39959'
+  }
 };
